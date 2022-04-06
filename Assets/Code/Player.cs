ce1fd1fbab47017eb0383b69b583c0378145b18e
Максим Высоцкий <visotskiy.m@gmail.com>
2022-04-06 11:38:48 +0300

    Player modify

--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -5,19 +5,35 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float _speed;
+    [SerializeField] private float _acceleration;
     [SerializeField] private float _hp;
     [SerializeField] private Rigidbody2D _bullet;
     [SerializeField] private Transform _barrel;
     [SerializeField] private float _force;
-    private MoveTransform _moveTransform;
+    private IMove _moveTransform;
 
     private void Start()
     {
-        _moveTransform = new MoveTransform(transform, _speed);
+        _moveTransform = new AccelerationMove(transform, _speed, _acceleration);
     }
     private void Update()
     {
         _moveTransform.Move(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), Time.deltaTime);
+
+        if(Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            if(_moveTransform is AccelerationMove accelerationMove)
+            {
+                accelerationMove.AddAcceleration();
+            }
+        }
+        if(Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            if(_moveTransform is AccelerationMove accelerationMove)
+            {
+                accelerationMove.RemoveAcceleration();
+            }
+        }
         
         if (Input.GetButtonDown("Fire1"))
         {
