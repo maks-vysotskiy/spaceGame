9c7baf2608c3dea2850a65f76ad1c5530288e880
Максим Высоцкий <visotskiy.m@gmail.com>
2022-04-06 11:33:25 +0300

    AccelerationMove + IMove

---
+++ Assets/Code/IMove.cs
@@ -0,0 +1,6 @@
+﻿public interface IMove
+{
+    float Speed { get; }
+    void Move(float horizontal, float vertical, float deltatime);
+}
+
--- Assets/Code/MoveTransform.cs
+++ Assets/Code/MoveTransform.cs
@@ -1,20 +1,21 @@
 ﻿using System;
 using UnityEngine;
-internal sealed class MoveTransform
+internal class MoveTransform: IMove
 {
     private readonly Transform _transform;
-    private readonly float _speed;
     private Vector3 _move;
+    public float Speed { get; protected set; }
 
     public MoveTransform(Transform transform, float speed)
     {
         _transform = transform;
-        _speed = speed;
+        Speed = speed;
     }
+        
 
     public void Move(float horizontal, float vertical, float deltaTime)
     {
-        var speed = _speed * deltaTime;
+        var speed = Speed * deltaTime;
         _move.Set(horizontal * speed, vertical * speed, 0.0f);
         _transform.localPosition += _move;
     }
