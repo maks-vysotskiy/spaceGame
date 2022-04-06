105f1326eee74b5d5056dce9dfebe340c05ccf40
Максим Высоцкий <visotskiy.m@gmail.com>
2022-04-06 11:19:25 +0300

    + MoveTransform

--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -9,19 +9,20 @@ public class Player : MonoBehaviour
     [SerializeField] private Rigidbody2D _bullet;
     [SerializeField] private Transform _barrel;
     [SerializeField] private float _force;
-    private Vector3 _move;
+    private MoveTransform _moveTransform;
 
+    private void Start()
+    {
+        _moveTransform = new MoveTransform(transform, _speed);
+    }
     private void Update()
     {
-        var deltaTime = Time.deltaTime;
-        var speed = _speed * deltaTime;
-        _move.Set(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed, 0.0f);
-        transform.localPosition += _move;
-
+        _moveTransform.Move(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), Time.deltaTime);
+        
         if (Input.GetButtonDown("Fire1"))
         {
             var temAmmunition = Instantiate(_bullet, _barrel.position, _barrel.rotation);
-            temAmmunition.AddForce(_barrel.up * _force * deltaTime);
+            temAmmunition.AddForce(_barrel.up * _force * Time.deltaTime);
         }
 
     }
